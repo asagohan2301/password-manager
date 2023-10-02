1b97e9dd044d566fd81646eed74a7793052dc134
asagohan2301 <shemomechama@yahoo.co.jp>
2023-10-02 14:32:47 +0000

    パスフレーズが間違っていた場合の処理を追加しました

--- password_manager.sh
+++ password_manager.sh
@@ -12,24 +12,37 @@ do
 
 	if [ "$selected_command" = "Add Password" ]; then
 
-		read -p "サービス名を入力してください：" service_name
-		read -p "ユーザー名を入力してください：" user_name
-		read -s -p "パスワードを入力してください：" password
-		echo
-		echo
-
 		passphrase="passphrase"
 		
 		# 暗号化されたファイルがすでにあれば復号化
 		if [ -e "$encrypt_file" ]; then
 			read -s -p "登録したパスフレーズを入力してください：" passphrase
 			echo
-			echo "$passphrase" | gpg --batch --passphrase-fd 0 -d --yes --output "$plaintext_file" "$encrypt_file"
+			echo
+			echo "$passphrase" | gpg --batch --passphrase-fd 0 -d --yes --output "$plaintext_file" "$encrypt_file" 2>/dev/null
+
+			# パスフレーズが間違っていた場合は後の処理に進まない
+			if [ $? -ne 0 ]; then
+				echo "パスフレーズが間違っています。"
+				echo
+				# rm "$plaintext_file"
+				continue
+			fi
+
 		# 初めてファイルを作成するとき
 		else
 			echo "パスフレーズを登録してください。"
 			read -s -p "今後情報を登録したり閲覧するときは、ここで登録したパスフレーズを入力してください：" passphrase
+			echo
+			echo
 		fi
+		
+		# 情報を入力してもらう
+		read -p "サービス名を入力してください：" service_name
+		read -p "ユーザー名を入力してください：" user_name
+		read -s -p "パスワードを入力してください：" password
+		echo
+		echo
 		
 		# 復号化したファイルに情報を追記
 		echo "${service_name}:${user_name}:${password}" >> "$plaintext_file"
@@ -40,7 +53,6 @@ do
 		# 暗号化する前のファイルを削除
 		rm "$plaintext_file"
 
-		echo
 		echo "パスワードの追加は成功しました。"
 		echo
 
@@ -49,7 +61,16 @@ do
 		# ファイルを復号化
 		read -s -p "登録したパスフレーズを入力してください：" passphrase
 		echo
-		echo "$passphrase" | gpg --batch --passphrase-fd 0 -d --yes --output "$plaintext_file" "$encrypt_file"
+		echo
+		echo "$passphrase" | gpg --batch --passphrase-fd 0 -d --yes --output "$plaintext_file" "$encrypt_file" 2>/dev/null
+
+		# パスフレーズが間違っていた場合は後の処理に進まない
+		if [ $? -ne 0 ]; then
+			echo "パスフレーズが間違っています。"
+			echo
+			# rm "$plaintext_file"
+			continue
+		fi
 		
 		# 復号化したファイルから情報を検索
 		read -p "サービス名を入力してください：" input_service_name
